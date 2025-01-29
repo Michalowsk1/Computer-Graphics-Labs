cb6a99a419901a315d86d3ffa90340a0f6d47c96
Michalowsk1 <23619282@stu.mmu.ac.uk>
2025-01-29 14:00:10 +0000

    Class Work

--- Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
+++ Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+void main()
+{
+	float angleInDegrees = 45.0f;
+	float pi = 3.141;
+
+	float angleInRadians = angleInDegrees * pi / 180.0f;
+
+
+
+
+
+
+
+
+	cout << "Hello World\n" << endl;
+	cout << angleInRadians << endl;
+}
