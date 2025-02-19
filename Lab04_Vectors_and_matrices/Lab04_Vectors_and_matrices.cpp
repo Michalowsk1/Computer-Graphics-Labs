a117e040e3fcb4b019b8e0c61918033a3380ae0c
Michalowsk1 <23619282@stu.mmu.ac.uk>
2025-02-19 11:19:18 +0000

    lab 4 finished

--- Lab04_Vectors_and_matrices/Lab04_Vectors_and_matrices.cpp
+++ Lab04_Vectors_and_matrices/Lab04_Vectors_and_matrices.cpp
@@ -13,9 +13,9 @@ int main()
     printf("--------------------------------------------------\n");
 
     //define vectors
-    vec3 a, b;
+    /*vec3 a, b;
     a = vec3(3.0f, 0.0f, 4.0f);
-    b[0] = 1.0f, b[1] = 2.0f, b[2] = 3.0f;
+    b[0] = 1.0f, b[1] = 2.0f, b[2] = 3.0f;*/
 
     //Vector length
     /*printf("\nVector Length:\n");
@@ -41,8 +41,55 @@ int main()
     /*cout << "2a   = " << 2.0f * a << endl;
     cout << "b / 3 = " << b / 3.0f << endl;*/
 
+    //Element multiplication
+   // cout << "a * b = " << a * b << endl;
+
     //Dot and Cross products
-    printf("\nDot and cross products:\n");
-    printf("a . b = %0.3f\n", dot(a, b));
+    /*printf("\nDot and cross products:\n");
+    printf("a . b = %0.3f\n", dot(a, b));*/
+
+    //cout << "a x b = " << cross(a, b) << endl;
+    //printf("a . (a x b) = %0.3f\n", dot(a, cross(a, b)));
+    //printf("b . (a x b) = %0.3f\n", dot(b, cross(a, b)));
+
+    //Defining matrices
+    mat2 A, B;
+
+    A[0][0] = 1.0f, A[0][1] = 2.0f,
+    A[1][0] = 3.0f, A[1][1] = 4.0f;
+
+    B = mat2(5.0f, 6.0f, 7.0f, 8.0f);
+
+    /*printf("\Defining matricies\n");
+    cout << "A = " << A << "\n" << endl;
+    cout << "B = " << B << "\n" << endl;*/
+
+    /*cout << "A = " << transpose(A) << "\n" << endl;
+    cout << "B = " << transpose(B) << "\n" << endl;*/
+
+    //Arithmetic operations on matricies
+    /*printf("Arithmetic operations on matricies:\n");
+    cout << "A + B = " << transpose(A + B) << "\n" << endl;
+    cout << "A - B = " << transpose(A - B) << "\n" << endl;
+    cout << "2A = " << transpose(2.0f * A) << "\n" << endl;
+    cout << "A / 3 = " << transpose(A / 3.0f) << "\n" << endl;*/
+
+    //Matrix Multiplication
+    /*cout << "A * B = " << transpose(B * A) << "\n" << endl;
+    cout << "B * A = " << transpose(A * B) << "\n" << endl;*/
+
+    //The Identity Matrix
+    /*printf("\nIdentity Matrix:\n");
+    mat4 I;
+    cout << "I =" << transpose(I) << "\n" << endl;*/
+
+    //Inverse matrices
+    printf("Inverse Matrices:\n");
+    mat2 invA = inverse(A);
+    mat2 invB = inverse(B);
+    cout << "invA = " << transpose(invA) << "\n" << endl;
+    cout << "invB = " << transpose(invB) << "\n" << endl;
+    cout << "invA * A = " << transpose(invA * A) << "\n" << endl;
+    cout << "invB * B = " << transpose(invB * B) << "\n" << endl;
     return 0;
 }
